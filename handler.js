b05e81fb098f5a5ebaf3ba552504c2f547ec8fe1
Tylor Shin <shincode@icloud.com>
2018-03-13 13:43:06 +0900

    Add path handling logic

--- handler.js
+++ handler.js
@@ -1,14 +1,22 @@
 "use strict";
 
 module.exports.redirect = (event, context, callback) => {
-  console.log(event);
+  const LAMBDA_SERVICE_NAME = "pluto-redirector";
+  const path = event.path;
+
+  let requestPath;
+  if (path === `/${LAMBDA_SERVICE_NAME}`) {
+    requestPath = "/";
+  } else {
+    requestPath = path.replace(`/${LAMBDA_SERVICE_NAME}`, "");
+  }
 
   const response = {
     statusCode: 301,
     headers: {
-      Location: "https://search.pluto.network",
+      Location: `https://search.pluto.network${requestPath}`,
     },
-    body: "https://search.pluto.network",
+    body: `https://search.pluto.network${requestPath}`,
   };
 
   context.succeed(response);
